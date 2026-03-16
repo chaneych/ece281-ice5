54c64221a7655226d4b392d0f19efc5e35dc3184
chaneych <c28cullen.chaney@afacademy.af.edu>
2026-03-16 09:00:29 -0600

    I think I found the problem. My clk was wrong, I had an elsif statement inside of the wrong if block.

--- src/elevator_controller_fsm.vhd
+++ src/elevator_controller_fsm.vhd
@@ -133,8 +133,8 @@ begin
 	   if rising_edge(i_clk) then
 	       if (i_reset = '1') then
 	           f_Q <= s_floor2;
-           elsif (i_reset = '0') then
-               f_Q <= f_Q_next;
+       elsif (i_stop = '0') then
+           f_Q <= f_Q_next;
            end if;
        end if;
     end process;
